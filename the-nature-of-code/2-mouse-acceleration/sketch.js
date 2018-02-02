3edb01f2e016a6b5c125216f110d689a800f6319
redxtreme <zahopper@mtu.edu>
2018-02-01 22:51:43 -0600

    orbit kind works

---
+++ the-nature-of-code/2-mouse-acceleration/sketch.js
@@ -0,0 +1,39 @@
+var w;
+
+// Setup function required by p5
+function setup() {
+  createCanvas(640, 360);
+  w = new Walker();
+
+}
+
+// Animation loop
+function draw() {
+  background(51);
+  w.update();
+  w.display();
+}
+
+function Walker() {
+  this.pos = createVector(width / 2, height / 2);
+
+  //velocity
+  this.vel = createVector(0, 0);
+
+  this.update = function() {
+    var mouse = createVector(mouseX, mouseY);
+
+    //Make a vector that goes from walker to the mouse
+    this.acc = p5.Vector.sub(mouse, this.pos);
+    this.acc.mult(0.01);
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+  }
+
+  //draw walker
+  this.display = function() {
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, 48, 48);
+  }
+}

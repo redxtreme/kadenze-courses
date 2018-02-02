401815e36efd92c08f972f9090ac61eba92640fe
redxtreme <zahopper@mtu.edu>
2018-02-01 22:03:36 -0600

    Added walker barebones

--- the-nature-of-code/random-walker/sketch.js
+++ the-nature-of-code/random-walker/sketch.js
@@ -12,3 +12,13 @@ function draw() {
   background(51);
   w.display();
 }
+
+function Walker() {
+  this.x = width/2;
+  this.y = height/2;
+
+  this.display = function() {
+    fill(255);
+    ellipse(this.x, this.y, 48, 48);
+  }
+}

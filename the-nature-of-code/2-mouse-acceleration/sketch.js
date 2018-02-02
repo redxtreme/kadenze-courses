6534d0eb541fb0cd4e9585d29ac1d227384a5ec2
redxtreme <zahopper@mtu.edu>
2018-02-01 23:10:17 -0600

    Random Perlin noise added

--- the-nature-of-code/2-mouse-acceleration/sketch.js
+++ the-nature-of-code/2-mouse-acceleration/sketch.js
@@ -1,3 +1,5 @@
+var xoff = 0;
+
 // Setup function required by p5
 function setup() {
   createCanvas(640, 360);
@@ -7,10 +9,10 @@ function setup() {
 function draw() {
   background(51);
 
-  var time = 0;
-
   //draw noise, needs a value of time
-  var x = noise(time) * width;
+  var x = noise(xoff) * width;
+
+  xoff += 0.05;
 
   fill(255);
   ellipse(x, 180, 48, 48);

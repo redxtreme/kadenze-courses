cadbc283ce06152263fe1a3de22016cc893f23ec
redxtreme <zahopper@mtu.edu>
2018-02-06 20:30:14 -0600

    add and delete

--- the-nature-of-code/7-particle-array/sketch.js
+++ the-nature-of-code/7-particle-array/sketch.js
@@ -2,11 +2,22 @@ var particles = [];
 
 function setup() {
   createCanvas(640, 360);
-  for (var i = 0; i < 50; i++) {
-    particles[i] = new Particle(random(width), 100, random(2, 4));
-  }
+  // for (var i = 0; i < 50; i++) {
+  //   particles[i] = new Particle(random(width), 100, random(2, 4));
+  // }
 }
 
+function mousePressed() {
+  var p = new Particle(mouseX, mouseY, random(2, 4));
+  particles.push(p);
+}
+
+function keyPressed() {
+
+  // If space is pressed, delete the oldest particle
+  if (key === ' ')
+    particles.splice(0,1);
+}
 function draw() {
   background(51);
   var wind = createVector(0.1, 0);

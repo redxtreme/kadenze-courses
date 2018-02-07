629903be83c7f4f8ac9d1f2a27152cfda598fc5d
redxtreme <zahopper@mtu.edu>
2018-02-06 20:25:36 -0600

    50 particles generated

--- the-nature-of-code/7-particle-array/sketch.js
+++ the-nature-of-code/7-particle-array/sketch.js
@@ -1,30 +1,26 @@
-var particle;
+var particles = [];
 
 function setup() {
   createCanvas(640, 360);
-  particle1 = new Particle(200, 100, 3);
-  particle2 = new Particle(400, 100, 1);
+  for (var i = 0; i < 50; i++) {
+    particles[i] = new Particle(random(width), 100, random(2, 4));
+  }
 }
 
 function draw() {
   background(51);
   var wind = createVector(0.1, 0);
 
-  var gravity1 = createVector(0, 0.2 * particle1.mass);
-  var gravity2 = createVector(0, 0.2 * particle2.mass);
+  for (var i = 0; i < particles.length; i++) {
+    var gravity = createVector(0, 0.2 * particles[i].mass);
 
-  // Apply forces
-  particle1.applyForce(gravity1);
-  particle2.applyForce(gravity2);
-  if (mouseIsPressed) {
-    particle1.applyForce(wind);
-    particle2.applyForce(wind);
+    // Apply forces
+    particles[i].applyForce(gravity);
+    if (mouseIsPressed)
+      particles[i].applyForce(wind);
+
+    particles[i].update();
+    particles[i].edges();
+    particles[i].display();
   }
-
-  particle1.update();
-  particle1.edges();
-  particle1.display();
-  particle2.update();
-  particle2.edges();
-  particle2.display();
 }
